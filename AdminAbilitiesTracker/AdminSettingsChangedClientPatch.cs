3e4ee0925b982712d29cc31a8cb04a012fac94f4
Pas2704 <71668530+Pas2704@users.noreply.github.com>
2022-06-16 10:22:31 -0400

    Add project files.

---
+++ AdminAbilitiesTracker/AdminSettingsChangedClientPatch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using Sandbox.Game.Gui;
+using Sandbox.Game.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRageMath;
+
+namespace AdminAbilitiesTracker
+{
+    [HarmonyPatch(typeof(MyGuiScreenAdminMenu), "AdminSettingsChangedClient")]
+    internal static class AdminSettingsChangedClientPatch
+    {
+        
+        [HarmonyPrefix]
+        public static void OnSettingsUpdated(AdminSettingsEnum settings, ulong steamId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            bool didResolve = MySession.Static.Players.TryGetPlayerBySteamId(steamId, out MyPlayer player);
+
+            if (didResolve)
+            {
+                sb.AppendLine($"Admin settings changed for {player.DisplayName} ({steamId})");
+
+            }
+            else
+            {
+                sb.AppendLine($"Unable to convert steamid to player id {steamId}");
+            }
+            var oldSettings = MySession.Static.RemoteAdminSettings[steamId];
+            bool didanyChanges = false;
+            foreach (Enum setting in Enum.GetValues(typeof(AdminSettingsEnum)))
+            {
+                bool hasChanged = oldSettings.HasFlag(setting) != settings.HasFlag(setting);
+                Console.WriteLine($"{oldSettings.HasFlag(setting)} - {settings.HasFlag(setting)}");
+                if (hasChanged)
+                {
+                    sb.AppendLine($"{setting} - was: {oldSettings.HasFlag(setting)}, now: {settings.HasFlag(setting)}");
+                    didanyChanges = true;
+                }
+            }
+
+            if (didanyChanges)
+            {
+                Console.WriteLine(sb);
+                MyHud.Chat.ShowMessage("Admin Abuse Detection", sb.ToString(), Color.White, "Green");
+            }
+        }
+    }
+}
---
+++ AdminAbilitiesTracker/Plugin.cs
@@ -0,0 +1,77 @@
+﻿using HarmonyLib;
+using NLog;
+using Sandbox.Game.Gui;
+using Sandbox.Game.Multiplayer;
+using Sandbox.Game.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI;
+using VRage.Input;
+using VRage.ModAPI;
+using VRage.Plugins;
+using VRage.Utils;
+using VRageMath;
+using VRageRender;
+
+namespace AdminAbilitiesTracker
+{
+    public class Plugin : IPlugin, IDisposable
+    {
+        public static Harmony Harmony
+        {
+            get
+            {
+                return new Harmony("AdminAbilitiesTracker");
+            }
+        }
+
+        public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static StringBuilder sb = new StringBuilder();
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Init(object gameInstance)
+        {
+            Log.Debug($"AdminAbilitiesTracker: Patching");
+            Harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Log.Info($"AdminAbilitiesTracker: Patches applied");
+        }
+        public void Dispose()
+        {
+
+        }
+        public void Update()
+        {
+            if (MySession.Static != null && MyRenderProxy.DrawRenderStats == MyRenderProxy.MyStatsState.SimpleTimingStats)
+            {
+                //Borrowed stuff from keen's debug menu
+                IMyCamera camera = ((IMySession)MySession.Static).Camera;
+                Vector2 vector2_1 = new Vector2(camera.ViewportSize.X * 0.01f, camera.ViewportSize.Y * 0.3f);
+                float num1 = 0.65f * Math.Min(camera.ViewportSize.X / 1920f, camera.ViewportSize.Y / 1200f);
+                foreach (var kvp in MySession.Static.RemoteAdminSettings)
+                {
+                    if ((kvp.Value & AdminSettingsEnum.AdminOnly) > AdminSettingsEnum.None)
+                    {
+                        string playerName;
+                        if (MySession.Static.Players.TryGetPlayerBySteamId(kvp.Key, out MyPlayer player))
+                        {
+                            playerName = player.DisplayName;
+                        }
+                        else
+                        {
+                            playerName = kvp.Key.ToString();
+                        }
+                        sb.AppendLine($"{playerName} - {kvp.Value}");
+                    }
+                }
+                MyRenderProxy.DebugDrawText2D(vector2_1, sb.ToString(), Color.Yellow ,num1);
+                sb.Clear();
+            }
+        }
+        
+    }
+}
